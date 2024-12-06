6f388c39a0c9f5820034fb15869a57f0f561dba6
Crafton <john.crafton@gmail.com>
2024-12-06 11:31:14 -0500

    Add basic requirements and config items
    Signed-off-by: Crafton <john.crafton@gmail.com>

---
+++ config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+APP_SECRET = os.getenv('APP_SECRET')
+
+MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT')
+MINIO_ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY')
+MINIO_SECRET_KEY = os.getenv('MINIO_SECRET_KEY')
+MINIO_BUCKET = os.getenv('MINIO_BUCKET')
